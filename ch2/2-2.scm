1025a0c9bc3cb25d8431250c6f663ef7b628fccf
Toru Takefusa <torutakefusa@gmail.com>
2019-08-20 07:39:30 +0900

    first commit

---
+++ ch2/2-2.scm
@@ -0,0 +1,38 @@
+(define (make-segment start-point end-point)
+  (cons start-point end-point))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define(y-point point)
+  (cdr point))
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (midpoint-segment segment)
+  (make-point (average (x-point (start-segment segment))
+                       (x-point (end-segment segment)))
+              (average (y-point (start-segment segment))
+                       (y-point (end-segment segment)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define p0 (make-point 0 0))
+(define p1 (make-point 4 4))
+(define seg1 (make-segment p0 p1))
+(print-point (midpoint-segment seg1))
