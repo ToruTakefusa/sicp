b7c8c3d9c59ac4b666d30878fc67207decf1a884
Toru Takefusa <torutakefusa@gmail.com>
2019-08-19 10:20:35 +0900

    move the all file of ch2 directory to ch1 directory. Files of ch2 directory are actually not belong to ch2. They are belong to ch1.

--- ch2/1-34.scm
+++
@@ -1,5 +0,0 @@
-(define (f g) (g 2))
-
-(print (f square))
-(print (f (lambda (z) ( * z (+ z 1)))))
-(print (f f))
--- ch2/1-41.scm
+++
@@ -1,9 +0,0 @@
-(define (double f)
-  (lambda (x)
-    (f (f x))))
-
-(define (inc n) (+ n 1))
-
-(print ((double inc) 1))
-
-(print (((double (double double)) inc) 5))
